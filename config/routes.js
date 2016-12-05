c76cc8f2c779e1a61d82bdea12d01eb71abdfae4
Darko <espadomkd@gmail.com>
2016-12-05 09:02:24 +0100

    serve index file

--- config/routes.js
+++ config/routes.js
@@ -1,8 +1,17 @@
 var config = require('./config');
+var path = require('path');
 
 module.exports = function (app) {
 
+  // Index page
   app.get('/', function (req, res) {
-    res.sendFile(config.root + '/index.html');
+    res.sendFile(path.join(__dirname, '../public/index.html'));
   });
+
+  // Movies
+  app.get('/popular', function (req, res) {
+    // Fix this to serve actual data
+    res.send('Deez are popular moviezz');
+  });
+
 };
