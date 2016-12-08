1b2566c553661ab45b021bfc59fd34b7c9fa9072
Darko <espadomkd@gmail.com>
2016-12-08 15:59:02 +0100

    Other movies cards

--- public/src/js/components.js
+++ public/src/js/components.js
@@ -3,11 +3,26 @@ app
   templateUrl: '../../components/navbar.html',
   controller: function() {
     var vm = this;
-  }
+  },
+  controllerAs: 'vm'
 })
 .component('featuredContent', {
   templateUrl: '../../components/featuredContent.html',
   controller: function () {
     var vm = this;
-  }
+  },
+  controllerAs: 'vm'
+})
+.component('resourceCard', {
+  templateUrl: '../../components/resourceCard.html',
+  bindings: {
+    resource: '='
+  },
+  controller: function () {
+    var vm = this;
+
+    console.log(vm.resource);
+
+  },
+  controllerAs: 'vm'
 })
--- public/src/js/controllers.js
+++ public/src/js/controllers.js
@@ -4,7 +4,56 @@ app.controller('HomePageController', function ($state) {
   $state.go('home.main');
 })
 
-.controller('HomeMainController', function ($state) {
+.controller('HomeMainController', function ($state, $http) {
   var vm = this;
+
+  vm.resources = [
+    {
+      title: 'Game of Thrones',
+      caption: 'Nine noble families fight for control over the mythical lands of Westeros. Meanwhile, a forgotten race, hell-bent on destruction, returns after being dormant for thousands of years.',
+      preview: 'http://www.geek.com/wp-content/uploads/2016/07/nedstark-625x350.jpg',
+    },
+    {
+      title: 'Game of Thrones',
+      caption: 'Nine noble families fight for control over the mythical lands of Westeros. Meanwhile, a forgotten race, hell-bent on destruction, returns after being dormant for thousands of years.',
+      preview: 'http://www.geek.com/wp-content/uploads/2016/07/nedstark-625x350.jpg',
+    },
+    {
+      title: 'Game of Thrones',
+      caption: 'Nine noble families fight for control over the mythical lands of Westeros. Meanwhile, a forgotten race, hell-bent on destruction, returns after being dormant for thousands of years.',
+      preview: 'http://www.geek.com/wp-content/uploads/2016/07/nedstark-625x350.jpg',
+    },
+    {
+      title: 'Game of Thrones',
+      caption: 'Nine noble families fight for control over the mythical lands of Westeros. Meanwhile, a forgotten race, hell-bent on destruction, returns after being dormant for thousands of years.',
+      preview: 'http://www.geek.com/wp-content/uploads/2016/07/nedstark-625x350.jpg',
+    },
+    {
+      title: 'Game of Thrones',
+      caption: 'Nine noble families fight for control over the mythical lands of Westeros. Meanwhile, a forgotten race, hell-bent on destruction, returns after being dormant for thousands of years.',
+      preview: 'http://www.geek.com/wp-content/uploads/2016/07/nedstark-625x350.jpg',
+    },
+    {
+      title: 'Game of Thrones',
+      caption: 'Nine noble families fight for control over the mythical lands of Westeros. Meanwhile, a forgotten race, hell-bent on destruction, returns after being dormant for thousands of years.',
+      preview: 'http://www.geek.com/wp-content/uploads/2016/07/nedstark-625x350.jpg',
+    },
+    {
+      title: 'Game of Thrones',
+      caption: 'Nine noble families fight for control over the mythical lands of Westeros. Meanwhile, a forgotten race, hell-bent on destruction, returns after being dormant for thousands of years.',
+      preview: 'http://www.geek.com/wp-content/uploads/2016/07/nedstark-625x350.jpg',
+    },
+    {
+      title: 'Game of Thrones',
+      caption: 'Nine noble families fight for control over the mythical lands of Westeros. Meanwhile, a forgotten race, hell-bent on destruction, returns after being dormant for thousands of years.',
+      preview: 'http://www.geek.com/wp-content/uploads/2016/07/nedstark-625x350.jpg',
+    },
+    {
+      title: 'Game of Thrones',
+      caption: 'Nine noble families fight for control over the mythical lands of Westeros. Meanwhile, a forgotten race, hell-bent on destruction, returns after being dormant for thousands of years.',
+      preview: 'http://www.geek.com/wp-content/uploads/2016/07/nedstark-625x350.jpg',
+    },
+    
+  ]
 
 })
